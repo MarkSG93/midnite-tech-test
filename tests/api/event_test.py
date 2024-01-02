4ce7ccbd532df6ab2aeaaf2ea306696282725e4d
Mark Griffiths <mgriffiths3193@gmail.com>
2024-01-02 19:19:58 +0000

    Green test responds with no alert

--- tests/api/event_test.py
+++ tests/api/event_test.py
@@ -10,7 +10,7 @@ def event():
     if event_type != 'deposit' and event_type != 'withdraw':
         return abort(400, "Only 'deposit' or 'withdraw' are supported event types")
     if "user_id" in content:
-        return jsonify(user_id=content["user_id"])
+        return jsonify(user_id=content["user_id"], alert_codes=[], alert=False)
     return ""
 
 def test_event_accepts_payload():
@@ -41,16 +41,6 @@ def test_event_accepts_valid_event_types(input: str):
         "type": input
     })
     assert response.status_code == 200
-
-def test_responds_with_payload_with_user_id():
-    response = app.test_client().post("/event", json={
-        "type": "deposit",
-        "amount": "42.00",
-        "user_id": 1,
-        "t": 10
-    })
-    json = response.get_json()
-    assert json['user_id'] == 1
 
 def test_responds_with_no_alert():
     response = app.test_client().post("/event", json={
